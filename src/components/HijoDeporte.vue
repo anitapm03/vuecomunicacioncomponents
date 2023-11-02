f1ea155060b66875c933014263a6eef926d3025f
anitapm03 <apereiramolinero@gmail.com>
2023-11-02 11:54:25 +0100

    padre e hijo

---
+++ src/components/HijoDeporte.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1 style="color: red">Hijo {{nombredeporte}}</h1>
+
+        <button @click="seleccionarFavHijo()">Favorito!</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HijoDeporte",
+        props: [ "nombredeporte" ],
+        methods: {
+            seleccionarFavHijo () {
+                console.log("Favorito selecionado");
+                //realizamos la llamada al parent
+                //tenemos la posibilidad de enviar parámetros 
+                //al metodo padre a continuación de la llamada 
+                //y separados por comas
+                this.$emit("seleccionarFavPadre", this.nombredeporte);
+
+            }
+        },
+        mounted (){
+            //console.log(this.nombredeporte);
+        }
+    }
+</script>
---
+++ src/components/PadreDeportes.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>Padre deportes</h1>
+        <h2 v-html="favorito" style="color: green"></h2>
+        <ul>
+            <li v-for="dep in deportes" :key="dep">
+                <!-- lleva : porque dep hace referencia a una variable -->
+                <HijoDeporte :nombredeporte="dep" v-on:seleccionarFavPadre="seleccionarFavPadre"/>
+            </li>
+        </ul>
+        
+    </div>
+</template>
+
+<script>
+import HijoDeporte from './HijoDeporte.vue'
+
+    export default {
+        name: "PadreDeportes",
+        components: {
+            HijoDeporte
+        },
+        methods:{
+            //vamos a crear un método sin recibir nada 
+            seleccionarFavPadre(nombre){
+                console.log("Favorito: " + nombre);
+                this.favorito = nombre;
+            }
+        },
+        data() {
+            return {
+                favorito: null,
+                deportes: ["Tenis", "Futbol", "Baloncesto", "Petanca", "Ping-pong"]
+            }
+        }
+    }
+</script>
